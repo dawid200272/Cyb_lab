4a3f1cd5c56f10106a12d1490611eb7691ea5aa8
dawid200272 <45143323+dawid200272@users.noreply.github.com>
2024-10-16 15:05:07 +0200

    Add create new user action to admin

--- Cyb_lab/Controllers/AdminController.cs
+++ Cyb_lab/Controllers/AdminController.cs
@@ -1,11 +1,50 @@
-using Cyb_lab.Models;
+using Cyb_lab.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
 
 namespace Cyb_lab.Controllers;
+
+[Authorize("Administrator")]
 public class AdminController : Controller
 {
-	public AdminController()
+	private readonly UserManager<IdentityUser> _userManager;
+
+	public AdminController(UserManager<IdentityUser> userManager)
 	{
+		_userManager = userManager;
+	}
+
+	// add a new user account (with role 'User')
+	// change password policy options
+	// browse list of user accounts
+	// ...
+
+	[HttpGet]
+	public IActionResult AddUser()
+	{
+		return View();
+	}
+
+	[HttpPost]
+	public async Task<IActionResult> AddUser(AddUserViewModel viewModel)
+	{
+		if (!ModelState.IsValid)
+		{
+			return View(viewModel);
+		}
+
+		var newUser = new IdentityUser(viewModel.UserName);
+
+		var result = await _userManager.CreateAsync(newUser, viewModel.Password);
+
+		if (!result.Succeeded)
+		{
+			return View(viewModel);
+		}
+
+		await _userManager.AddToRoleAsync(newUser, UserRoles.User.ToString());
+
+		return RedirectToAction(nameof(HomeController.Index), "Home");
 	}
 }
---
+++ Cyb_lab/ViewModels/AddUserViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Cyb_lab.ViewModels;
+
+public class AddUserViewModel
+{
+	[Required]
+	[Display(Name = "User name")]
+	public string UserName { get; set;}
+
+	[Required]
+	[StringLength(maximumLength: 100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+	[DataType(DataType.Password)]
+	public string Password { get; set; }
+
+	[DataType(DataType.Password)]
+	[Display(Name = "Confirm password")]
+	[Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+	public string ConfirmPassword { get; set; }
+}
